0a0eb1d3ed97e2adaf39be488b7ef5d3f6646c98
bsturdyuk <b.sturdyuk@gmail.com>
2018-05-04 15:39:03 +0700

    First commit

---
+++ js/custom.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  // Nav Functionality
+ $('.menu-toggle').on('click', function(){
+   $('.full-menu').toggleClass('full-menu--open');
+ });
+
+ $('button#nav-icon').click(function(){
+   $(this).toggleClass('full-menu--open');
+ });
+
+
+ $(".parent-link").click(function() {
+    var value = $(this).attr('href');
+
+    $('html, body').animate({
+        scrollTop: $(value).offset().top
+    }, 1000);
+
+    $('.full-menu').toggleClass('full-menu--open');
+    $('button#nav-icon').toggleClass('full-menu--open');
+});
+});
